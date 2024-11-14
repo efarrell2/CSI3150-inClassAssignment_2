ac3d13d259e96779898a801a65f7512adea4f2c9
Emma Farrell <efarrell@oakland.edu>
2024-11-14 15:31:28 -0500

    javascript finalized

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function showA(){
+    document.getElementById('A').style.visibility='visible';
+}
+
+function hideA(){
+    document.getElementById('A').style.visibility='hidden';
+}
+
+function showB(){
+    document.getElementById('B').style.visibility='visible';
+}
+
+function hideB(){
+    document.getElementById('B').style.visibility='hidden';
+}
+
+function showC(){
+    document.getElementById('C').style.visibility='visible';
+}
+
+function hideC(){
+    document.getElementById('C').style.visibility='hidden';
+}
